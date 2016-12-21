b8f759607c8c62f89455ec1d3b60394a3c2ca581
Paul Gowder <paul.gowder@gmail.com>
2016-12-21 17:16:39 -0600

    median

--- src/curvecalc/upload/validate.cljs
+++ src/curvecalc/upload/validate.cljs
@@ -2,3 +2,12 @@
 
 (defn sorted-gradelist [column]
   (into [] (sort (map #(js/parseFloat %) (remove #(js/isNaN (js/parseFloat %)) column)))))
+
+(defn find-median
+"sorted numeric list -> string"
+  [sgl]
+  (let [c (count sgl)]
+  (if (even? c)
+    (str "median: " (nth sgl (- (/ c 2) 1)) "-" (nth sgl (/ c 2)))
+    (str "median: " (nth sgl (quot c 2)))
+    )))
